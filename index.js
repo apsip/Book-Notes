e1008722f8e405b24339a6c8ceae35cc0998ab8f
Andrew Sipovac <andrewsipovac@gmail.com>
2025-02-20 10:47:09 -0800

    Connected database and open covers API in js file and rendered data on ejs file

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 import express from "express";
 import bodyParser from "body-parser";
+import pg from "pg";
 
 const app = express();
 const port = 3000;
+
+const db = new pg.Client({
+    user: "postgres",
+    host: "localhost",
+    database: "my_books",
+    password: "Apsip1826",
+    port: 5432,
+  });
+  db.connect();
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -10,8 +20,16 @@ app.use(express.static("public"));
 
 
 // Basic Route
-app.get("/", (req, res) => {
-    res.render("index.ejs");
+app.get("/", async (req, res) => {
+    try {
+        const result = await db.query("SELECT title, author, date_read, rating, notes, isbn FROM books");
+        console.log(result.rows); // Log the books table to the console (for debugging)
+        
+        res.render("index.ejs", { books: result.rows }); // Send data to EJS template
+      } catch (error) {
+        console.error("Error fetching books:", error);
+        res.status(500).send("Database error");
+      }
 });
 
 
